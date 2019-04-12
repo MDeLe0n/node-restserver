b10113e4f426b17029e20bea7c2c43964ab5e542
Viridiana de Leon <viridiana.dlg@gmail.com>
2019-04-12 11:01:22 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,25 @@
 //==========================
 
 process.env.PORT = process.env.PORT || 3000;
+
+// =========================
+  //       ENTORNO
+//==========================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =========================
+  //     BASE DE DATOS
+//==========================
+
+let urlDB;
+ if (process.env.NODE_ENV === 'dev')
+ {
+   urlDB = 'mongodb://localhost:27017/cafe';
+ }
+ else
+ {
+  urlDB = 'mongodb+srv://montserrat:Oqsm0W0RLYIhaNSR@cluster0-wflwm.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
